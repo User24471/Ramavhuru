375e3e4e7fd0a87a5c65d904671f8d6e911f7521
User24471 <lukhwarenithendo60@gmail.com>
2025-10-24 15:59:50 +0200

    k

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,12 @@
       icon.classList.toggle("fa-moon");
       icon.classList.toggle("fa-sun");
     });
+
+
+    const toggle = document.getElementById("darkModeToggle");
+    toggle.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+      const icon = toggle.querySelector("i");
+      icon.classList.toggle("fa-moon");
+      icon.classList.toggle("fa-sun");
+    });
